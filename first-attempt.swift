e005613ab2fcb84f89e3968fbef701d04dd05059
Styrbjörn Holmberg <styrbjorn.holmberg@gmail.com>
2020-07-30 18:01:46 +0200

    Create first-attempt.swift

---
+++ first-attempt.swift
@@ -0,0 +1,16 @@
+var totalTimesWon = 0
+
+var randomInt1 = Int.random(in:0..<10)
+
+var winningNumber = Int.random(in:0..<10)
+
+while (totalTimesWon < 10) { 
+    if winningNumber == randomInt1 {
+        totalTimesWon += 1}
+    winningNumber
+    randomInt1
+    
+}
+
+
+
